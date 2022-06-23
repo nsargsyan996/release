fe72443db2d6109bcdb7469c9b7747c28fc4205c
Narek <narek.sargsyan@inomma.com>
2022-06-23 18:05:04 +0400

    feat: release new feature

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
+          new feature
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
